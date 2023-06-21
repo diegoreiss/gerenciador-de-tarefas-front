c6f04af41c2c9eb0ae07acaf93549928f3a293aa
Diego Reis <105183365+Ghadrr@users.noreply.github.com>
2023-06-21 19:51:10 -0300

    add config_ip

---
+++ App/Src/Routers/config_ip.py
@@ -0,0 +1,31 @@
+import os
+from configparser import ConfigParser
+
+
+class ConfigIpAddress(ConfigParser):
+    def get_ip_address(self, section: str = 'api'):
+        parent_path = os.path.dirname(os.getcwd())
+        full_file_path = os.path.join(parent_path, 'api.ini')
+
+        print(full_file_path)
+        self.read(full_file_path)
+
+        if self.has_section(section):
+            params = self.items(section)
+
+            ini_params = {param[0]: param[1] for param in params}
+
+            return ConfigIpAddress.__format_ip_address(ini_params)
+
+    @staticmethod
+    def __format_ip_address(params: dict):
+        pattern = 'http://host:port'
+        pattern_copy = pattern[:]
+
+        for key, value in params.items():
+            pattern_copy = pattern_copy.replace(key, value)
+
+        return pattern_copy
+
+
+IP_ADDRESS = ConfigIpAddress().get_ip_address()
