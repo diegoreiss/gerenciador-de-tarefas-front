9f4d5846776fb99f64c8b3f937c427d32918f9fb
Diego Reis <105183365+Ghadrr@users.noreply.github.com>
2023-06-21 23:20:33 -0300

    add router

---
+++ App/Src/Routers/router.py
@@ -0,0 +1,9 @@
+import os
+from .config_ip import IP_ADDRESS
+
+
+class Router:
+    def __init__(self):
+        self._IP_ADDRESS = IP_ADDRESS
+        self._token = os.environ.get("TOKEN_DO_USUARIO")
+        self._token_type = os.environ.get("TOKEN_DO_USUARIO_TYPE")
