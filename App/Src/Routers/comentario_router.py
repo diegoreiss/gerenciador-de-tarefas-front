0851f9fbac1ce6bd9a2738e7f1b6e606311e8a40
diegoreiss <diegoportal.reis@outlook.com>
2023-06-28 03:10:03 -0300

    adicionando funcoes de atualizar e deletar comentarios

--- App/Src/Routers/comentario_router.py
+++ App/Src/Routers/comentario_router.py
@@ -28,3 +28,25 @@ class ComentarioRouter(Router):
             raise e
         except BaseException as e:
             raise e
+
+    def update_comentario(self, id: int, request_body: dict, endpoint: str = "/comentario/"):
+        url = f"{self._IP_ADDRESS}{endpoint}{id}"
+        headers = {"accept": "application/json", "Authorization": f"{self._token_type} {self._token}"}
+
+        try:
+            return requests.put(url, headers=headers, json=request_body)
+        except InvalidURL as e:
+            raise e
+        except BaseException as e:
+            raise e
+
+    def delete_comentario(self, id: int, endpoint: str = "/comentario/"):
+        url = f"{self._IP_ADDRESS}{endpoint}{id}"
+        headers = {"accept": "application/json", "Authorization": f"{self._token_type} {self._token}"}
+
+        try:
+            return requests.delete(url, headers=headers)
+        except InvalidURL as e:
+            raise e
+        except BaseException as e:
+            raise e
